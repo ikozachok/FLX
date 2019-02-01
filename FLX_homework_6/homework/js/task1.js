023bf04b4d9a26bebf7b9485359ef320fd9484ea
Iryna Kozachok <41184148+ikozachok@users.noreply.github.com>
2019-02-01 16:24:33 +0200

    Add files via upload

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,9 +1,45 @@
 const a = parseFloat(prompt('Please enter a for quadratic equation ax2 + bx + c = 0', 0));
 const b = parseFloat(prompt('Please enter b for quadratic equation ax2 + bx + c = 0', 0));
 const c = parseFloat(prompt('Please enter c for quadratic equation ax2 + bx + c = 0', 0));
-
-if (a || b || c <= 0) {
-  alert('Invalid input data')
+const d = b * b - 4 * a * c;
+console.log(d);
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  alert('Invalid input data');
 }
-
-console.log('dsds');
+else
+if (a == 0 && b == 0 && c == 0) {
+  const x1 = 'any number';
+  const x2 = 'any number';
+  console.log(x1);
+  console.log(x2);
+}
+else
+if(a==0 && b==0 && c!=0)
+{
+  const x1 = 'no solution';
+  const x2 = 'no solution';
+  console.log(x1);
+  console.log(x2);
+}
+else
+if (a == 0 && b != 0 && c != 0) {
+  const x = - c / b;
+  console.log(x);
+}
+else
+if (a != 0 && d > 0) {
+  const x1 = (- b + Math.sqrt(d))/(2 * a);
+  const x2 = (- b - Math.sqrt(d))/(2 * a);
+  console.log(x1);
+  console.log(x2);
+}
+else
+if (a != 0 && d == 0) {
+  const x = (- b) / ( 2 * a );
+  console.log(x);
+}
+else
+if (d < 0) {
+  console.log('no solution')
+}
+/*console.log('dsds');*/
