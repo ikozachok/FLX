369761dea0e9890d69f5b2049092a34eb2cc14a4
Iryna Kozachok <41184148+ikozachok@users.noreply.github.com>
2019-02-01 16:58:42 +0200

    Add files via upload

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,45 +1,44 @@
 const a = parseFloat(prompt('Please enter a for quadratic equation ax2 + bx + c = 0', 0));
 const b = parseFloat(prompt('Please enter b for quadratic equation ax2 + bx + c = 0', 0));
 const c = parseFloat(prompt('Please enter c for quadratic equation ax2 + bx + c = 0', 0));
+
 const d = b * b - 4 * a * c;
-console.log(d);
+
+let x1 = 'no solution';
+let x2 = 'no solution';
+
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
   alert('Invalid input data');
 }
 else
 if (a == 0 && b == 0 && c == 0) {
-  const x1 = 'any number';
-  const x2 = 'any number';
-  console.log(x1);
-  console.log(x2);
+  x1 = 'any number';
+  x2 = 'any number';
 }
 else
 if(a==0 && b==0 && c!=0)
 {
-  const x1 = 'no solution';
-  const x2 = 'no solution';
-  console.log(x1);
-  console.log(x2);
+  x1 = 'no solution';
+  x2 = 'no solution';
 }
 else
 if (a == 0 && b != 0 && c != 0) {
-  const x = - c / b;
-  console.log(x);
+  x1 = - c / b;
+  x2 = x1;
 }
 else
 if (a != 0 && d > 0) {
-  const x1 = (- b + Math.sqrt(d))/(2 * a);
-  const x2 = (- b - Math.sqrt(d))/(2 * a);
-  console.log(x1);
-  console.log(x2);
+  x1 = (- b + Math.sqrt(d))/(2 * a);
+  x2 = (- b - Math.sqrt(d))/(2 * a);
 }
 else
 if (a != 0 && d == 0) {
-  const x = (- b) / ( 2 * a );
-  console.log(x);
+  x1 = (- b) / ( 2 * a );
+  x2 = x1;
 }
 else
 if (d < 0) {
-  console.log('no solution')
+  x1 = 'no solution';
+  x2 = 'no solution';
 }
-/*console.log('dsds');*/
+console.log(x1, x2)
