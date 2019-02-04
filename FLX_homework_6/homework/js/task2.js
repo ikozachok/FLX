7d5b84ec35df49f36f3a54829f792c31de555f2a
Iryna Kozachok <41184148+ikozachok@users.noreply.github.com>
2019-02-04 12:51:52 +0200

    Add files via upload

--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -4,7 +4,7 @@ const discount = parseFloat(prompt('Please enter discount', 0));
 if (amount >= 0 && discount <= 100) {
   const total = Math.round(amount * (1 - discount/100) * 100)/100;
   const saved = Math.round((amount - total)*100)/100;
-  alert.log(
+  alert(
     'Price without discount: ' + amount + '\r\n' +
     'Discount: ' + discount + '%\r\n' +
     'Price with discount: ' + total + '\r\n' +
