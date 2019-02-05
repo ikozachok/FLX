44f60d8387bb3aae8fa7019b78b0ae8630b3eb9d
Iryna Kozachok <41184148+ikozachok@users.noreply.github.com>
2019-02-05 21:54:25 +0200

    Add files via upload

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -4,34 +4,17 @@ const c = parseFloat(prompt('Please enter c for quadratic equation ax2 + bx + c 
 
 const d = b * b - 4 * a * c;
 
-let value1 = 'no solution';
-let value2 = 'no solution';
-
-if (isNaN(a) || isNaN(b) || isNaN(c)) {
-  alert('Invalid input data');
-  } else
-if (a === 0 && b === 0 && c === 0) {
-  value1 = 'any number';
-  value2 = value1;
-  } else
-if (a === 0 && b === 0 && c !== 0) {
-  value1 = 'no solution';
-  value2 = value1;
-  } else
-if (a === 0 && b !== 0 && c !== 0) {
-  value1 = - c / b;
-  value2 = ' ';
-  alert('x = ' + value1);
- } else
-if (a !== 0 && d > 0) {
-  value1 = (- b + Math.sqrt(d))/(2 * a);
-  value2 = (- b - Math.sqrt(d))/(2 * a);
-  } else
 if (a !== 0 && d === 0) {
-  value1 = (- b) / ( 2 * a );
-  value2 = value1;
+  const value = (- b) / ( 2 * a );
+  alert('x = ' + value);
+  } else
+if (a !== 0 && d > 0) {
+  const value1 = (- b + Math.sqrt(d))/(2 * a);
+  const value2 = (- b - Math.sqrt(d))/(2 * a);
+  alert('x1 = ' + value1 + ' and x2 =' + value2);
   } else
 if (d < 0) {
-  value1 = 'no solution';
+  alert('no solution');
+} else {
+  alert('Invalid input data');
 }
-alert('x1 = ' + value1 + ' and x2 =' + value2)
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,7 +1,7 @@
 const amount = parseFloat(prompt('Please enter price', 0));
 const discount = parseFloat(prompt('Please enter discount', 0));
 
-if (amount >= 0 && discount <= 100) {
+if (amount >= 0 && amount <= 9999999 && discount <= 99 && discount >= 0) {
   const total = Math.round(amount * (1 - discount/100) * 100)/100;
   const saved = Math.round((amount - total)*100)/100;
   alert(
